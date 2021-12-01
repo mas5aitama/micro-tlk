f6050170cf05888be6f6d8c2e352f4e5e41e6f02
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2021-12-01 21:25:18 +0700

    Microservice Eureka Discovery

---
+++ pbs-eureka-discovery/src/test/java/com/eurekapandhuta/discovery/PandhutaEurekaDiscovery/PandhutaEurekaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekapandhuta.discovery.PandhutaEurekaDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandhutaEurekaDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
